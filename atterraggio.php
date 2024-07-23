1f7943240e3a460720710d13ee93f4a9a19ce352
simone menichetti <simon3menichettui95@gmail.com>
2024-07-23 15:39:04 +0200

    feat:(add var for Paragraph Length and censored paragraph stamp in page paragraph origin and censored)

--- atterraggio.php
+++ atterraggio.php
@@ -8,8 +8,28 @@ $parola = $_POST["parola"];
 
 $lunghezzaParagrafo = strlen("paragrafo");
 
+// spstituzione parola da censurare con ***
 
+$paragrafoParolacensurata=str_replace($parola,'***',$paragrafo);
+
+// Calcolo della lunghezza del paragrafo censurato
+$lunghezzaparagrafocensurato=strlen("paragrafoParolacensurata");
+
+// stampiamo in pagina 
+
+// section paragrafo originale non censurato
+echo "<h2>Paragrafo originale:</h2>";
+// mostriamo il paragrafo originale 
 echo "<p>$paragrafo</p>";
+// stampiamo la lunghezza del parafgrafo
 echo "<p>Lunghezza: $lunghezzaParagrafo caratteri</p>";
 
+// section paragrafo censurato 
+
+// titolo
+echo "<h2>Paragrafo parola censurata:</h2>";
+// censuriamo il paragrafo
+echo "<p>$paragrafoParolacensurata</p>";
+// stampiamo la lunghezza del parafgrafo
+echo "<p>Lunghezza:$lunghezzaparagrafocensurato caratteri</p>";
 ?>
